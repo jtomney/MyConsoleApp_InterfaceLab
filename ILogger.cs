1f7ff649d3d9b9f0c7d668652e3f300b178c2441
Jim Tomney <jim@tomney.com>
2021-06-08 15:59:46 -0400

    Add project files.

---
+++ ILogger.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public interface ILogger
+{
+    void LogMsg(string msg);
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using MyConsoleApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    public class Program
+    {
+        private static ILogger _logger;
+
+        static void Main(string[] args)
+        {
+            ComposeApp();
+            _logger.LogMsg("Hello World");
+            WaitToExit();
+        }
+
+        private static void ComposeApp()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WaitToExit()
+        {
+            Console.WriteLine("All done, you can exit now");
+            Console.ReadLine();
+        }
+    }
+}
