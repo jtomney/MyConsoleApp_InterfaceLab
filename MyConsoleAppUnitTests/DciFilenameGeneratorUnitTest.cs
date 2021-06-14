91088cd47267b8717ee3a41264811fc47ac7f577
kimballdubs <kimballdubs@gmail.com>
2021-06-14 13:34:18 -0400

    Unit Test Complete

---
+++ MyConsoleAppUnitTests/DciFilenameGeneratorUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using MyConsoleApp.Interfaces;
+using MyConsoleApp.UniqueFilenameClass;
+using MyConsoleAppUnitTests.Fake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyConsoleAppUnitTests
+{
+    public class DciFilenameGeneratorUnitTest
+    {
+        [Fact]
+        public void ShouldGenerateFilenameThatStartsWithServerNameAppId()
+        {
+            //Arrange
+            string inputServerName = "S3B4E02";
+            string inputAppId = "JT2";
+            string expectedStartWith = "S3B4E02JT2";
+            ILogger logger = new FakeLogger();
+            IDciFilenameGenerator sut = new DciFilenameGenerator(logger);
+
+            //Act
+            string result = sut.GenerateUniqueDciFilename(inputServerName, inputAppId);
+
+            //Assert
+            Assert.StartsWith(expectedStartWith, result);
+        }
+    }
+}
---
+++ MyConsoleAppUnitTests/Fake/FakeLogger.cs
@@ -0,0 +1,19 @@
+﻿using MyConsoleApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleAppUnitTests.Fake
+{
+    class FakeLogger : ILogger
+    {
+        List<string> _msgs = new List<string>();
+
+        public void LogMsg(string msg)
+        {
+            _msgs.Add(msg);
+        }
+    }
+}
