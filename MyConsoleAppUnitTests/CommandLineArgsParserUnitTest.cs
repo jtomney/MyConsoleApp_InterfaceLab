2a2b3658987a886b8ade8e473a8da8b81bdbd667
kimballdubs <kimballdubs@gmail.com>
2021-06-14 14:26:08 -0400

    Unit Text Complete. Ignore the previous commit. It was wrong files.

---
+++ MyConsoleAppUnitTests/CommandLineArgsParserUnitTest.cs
@@ -0,0 +1,66 @@
+﻿using MyConsoleApp.Interfaces;
+using MyConsoleApp.UniqueFilenameClass;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MyConsoleAppUnitTests
+{
+    public class CommandLineArgsParserUnitTest
+    {
+        /// <summary>
+        /// Given:  A command line string array containing one item with a valid Server Name and AppID separated by a comma.
+        /// When: The plugin parses that array.
+        /// Then:  A single struct is returned whose Server Name property and Appid property are assigned their value from the array item.
+        /// </summary>
+        [Fact]
+        public void ShouldParseCmdLineArgsToDto()
+        {
+            //Arrange
+            string[] input = { "S3B4E02,JT2" };
+            ServerNameAppIdDto expected = new ServerNameAppIdDto()
+                                            { Servername = "S3B4E02", AppId = "JT2"};
+            ICommandLineArgsParser sut = new CommandLineArgsParser();
+
+            //Act
+            var result = sut.ParseCmdLineArgsToSrvNmAppIdDTOs(input);
+
+            //Assert
+            Assert.Equal(expected.Servername, result[0].Servername);
+            Assert.Equal(expected.AppId, result[0].AppId);
+        }
+
+        /// <summary>
+        /// Given: A command line string array containing multiple items with a 
+        ///        valid Server Name and AppID separated by a comma
+        /// When: The plugin parses that array
+        /// Then: structs are returned whose Server Name property and Appid property 
+        ///       are assigned their value from the array. 
+        /// </summary>
+        [Fact]
+        public void ShouldParseMultipleCmdLineArgsToDto()
+        {
+            //Arrange
+            string[] input = { "S3B4E02,JT2", "S4B5E03,KC3", "S4B6E04,SD4" };
+            List<ServerNameAppIdDto> expected = new List<ServerNameAppIdDto>()
+                        { new ServerNameAppIdDto() { Servername = "S3B4E02", AppId = "JT2" },
+                         new ServerNameAppIdDto() { Servername = "S4B5E03", AppId = "KC3" },
+                         new ServerNameAppIdDto() { Servername = "S4B6E04", AppId = "SD4" }};
+            ICommandLineArgsParser sut = new CommandLineArgsParser();
+
+            //Act
+            var result = sut.ParseCmdLineArgsToSrvNmAppIdDTOs(input);
+
+            //Assert
+            Assert.Equal(expected[0].Servername, result[0].Servername);
+            Assert.Equal(expected[0].AppId, result[0].AppId);
+
+            Assert.Equal(expected[1].Servername, result[1].Servername);
+            Assert.Equal(expected[1].AppId, result[1].AppId);
+
+            Assert.Equal(expected[2].Servername, result[2].Servername);
+            Assert.Equal(expected[2].AppId, result[2].AppId);
+        }
+    }
+}
--- MyConsoleAppUnitTests/DciFilenameGeneratorUnitTest.cs
+++ MyConsoleAppUnitTests/DciFilenameGeneratorUnitTest.cs
@@ -1,29 +1,56 @@
-﻿using MyConsoleApp.Interfaces;
+using MyConsoleApp.Interfaces;
 using MyConsoleApp.UniqueFilenameClass;
 using MyConsoleAppUnitTests.Fake;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace MyConsoleAppUnitTests
 {
-    public class DciFilenameGeneratorUnitTest
+    public class DciFileNameGeneratorUnitTest
     {
+        /// <summary>
+        /// Given: a valid Server Name and an AppID  as input
+        /// When: the plugin completes its processing
+        /// Then: the log file contains the file name that starts with the Server Name and AppID
+        /// </summary>
         [Fact]
-        public void ShouldGenerateFilenameThatStartsWithServerNameAppId()
+        public void ShouldCreateFileNameThatStartsWithServerNameAppId()
         {
             //Arrange
             string inputServerName = "S3B4E02";
-            string inputAppId = "JT2";
+            string inputAppID = "JT2";
             string expectedStartWith = "S3B4E02JT2";
             ILogger logger = new FakeLogger();
             IDciFilenameGenerator sut = new DciFilenameGenerator(logger);
 
             //Act
-            string result = sut.GenerateUniqueDciFilename(inputServerName, inputAppId);
+            string result = sut.GenerateUniqueDciFilename(inputServerName, inputAppID);
+
+            //Assert
+            Assert.StartsWith(expectedStartWith, result);            
+        }
+
+        /// <summary>
+        /// Given:  Multiple Server Name & AppID pairs as input
+        /// When:  The plugin completes processing
+        /// Then:  There is a unique file name for each input
+        /// </summary>
+        /// <param name="inputServerName"></param>
+        /// <param name="inputAppID"></param>
+        /// <param name="expectedStartWith"></param>
+        [Theory]
+        [InlineData("S3B4E02", "JT2", "S3B4E02JT2")]
+        [InlineData("S4B5E03", "KC3", "S4B5E03KC3")]
+        [InlineData("S4B6E04", "SD4", "S4B6E04SD4")]
+        public void ShouldCreateMultipleFileNameThatStartsWithServerNameAppId(string inputServerName,
+                                    string inputAppID, string expectedStartWith)
+        {
+            //Arrange
+            ILogger logger = new FakeLogger();
+            IDciFilenameGenerator sut = new DciFilenameGenerator(logger);
+
+            //Act
+            var result = sut.GenerateUniqueDciFilename(inputServerName, inputAppID);
 
             //Assert
             Assert.StartsWith(expectedStartWith, result);
